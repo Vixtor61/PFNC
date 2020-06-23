ec93e9114a4b27ea37afbdffd57204bc1664eb26
Vixtor61 <00198117@uca.edu.sv>
2020-06-23 17:40:31 -0600

    Initial Commit

---
+++ PFNC/src/main/java/com/uca/capas/pfnc/PfncApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas.pfnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfncApplication.class, args);
+	}
+
+}
---
+++ PFNC/src/test/java/com/uca/capas/pfnc/PfncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas.pfnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
