776b24f6e2317a85fc4e0d473fa032f3dc86b340
Vixtor61 <00198117@uca.edu.sv>
2020-06-27 00:00:12 -0600

    user details implementation

--- PFNC/src/main/java/com/uca/capas/pfnc/security/PrincipalUserDetails.java
+++ PFNC/src/main/java/com/uca/capas/pfnc/security/PrincipalUserDetails.java
@@ -1,45 +1,62 @@
 package com.uca.capas.pfnc.security;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.uca.capas.pfnc.domain.Cuenta;
+
 public class PrincipalUserDetails implements UserDetails{
+	private Cuenta user;
+	
+	public PrincipalUserDetails (Cuenta user){
+        this.user = user;
+        
+    }
+
 	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
-		return null;
+	      List<GrantedAuthority> authorities = new ArrayList<>();
+	      GrantedAuthority authority = new SimpleGrantedAuthority(this.user.getTipo());
+	      authorities.add(authority);
+
+
+	        return authorities;
 	}
 	@Override
 	public String getPassword() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.user.getPassword();
 	}
 	@Override
 	public String getUsername() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.user.getUsername();
 	}
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 	@Override
 	public boolean isAccountNonLocked() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
-		return false;
+		return this.user.getEstado();
 	}
 
 }
